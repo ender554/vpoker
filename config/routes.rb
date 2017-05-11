d36aa2ed91a756256aa4c0c5002e4e22ff5898df
ender554 <joshadams554@gmail.com>
2017-05-10 18:00:34 -0700

    added functionality to odds finder and started page for it to land

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get '/odds' => 'static_pages#odds'
+  get '/index' => 'static_pages#index'
 end
